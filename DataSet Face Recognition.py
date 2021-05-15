f1145bfe565f03aa260dda0fb3527507ca439675
Mani Singh <75898710+mani24singh@users.noreply.github.com>
2021-05-16 02:21:24 +0530

    Dataset Creation for face recognition
    How to create dataset for face recognition

---
+++ DataSet Face Recognition.py
@@ -0,0 +1,33 @@
+import cv2, os
+haar_file = "haarcascade_frontalface_default.xml"
+dataset = "Dataset-faceRecognition"
+sub_data = "Ram"     
+
+path = os.path.join(dataset,sub_data)
+if not os.path.isdir(path):
+    os.mkdir(path)
+(width,height) = (130,100)
+
+face_cascade = cv2.CascadeClassifier(haar_file)
+webcam = cv2.VideoCapture(0,cv2.CAP_DSHOW)
+    
+count = 1
+while count < 31:
+    print(count)
+    _,im = webcam.read()
+    gray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray,1.3,4)
+    for (x,y,w,h) in faces:
+        cv2.rectangle(im,(x,y),(x+w,y+h), (0,255,0),2)
+        face = gray[y:y+h,x:x+w]
+        face_resize = cv2.resize(face,(width,height))
+        cv2.imwrite("%s/%s.jpg"%(path,count),face_resize)
+        count+=1
+    cv2.imshow("OpenCV",im)
+    key = cv2.waitKey(10)
+    if key == 27:
+        break
+print("Face recognised successfully")
+
+cam.release()
+cv2.destroyAllWindows()
